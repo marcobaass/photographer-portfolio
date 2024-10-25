84647e9386f11ca1e4f9c1333d8e886b81125448
Marco Baass <picture@marcobaass.com>
2024-10-25 14:36:10 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import node from '@astrojs/node';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
