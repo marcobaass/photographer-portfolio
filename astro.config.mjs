3ed423f46c97020efe03ca52933ff88b9a4b2eff
Marco Baass <picture@marcobaass.com>
2024-11-29 12:54:54 +0100

    starting footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,11 @@ import node from '@astrojs/node';
 
 import tailwind from '@astrojs/tailwind';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), icon()],
   output: 'server',
 
   adapter: node({
