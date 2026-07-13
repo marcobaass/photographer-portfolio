d4ac9a61aa8d2a86f3f53be8cf3bf2cbaf7e5a3e
Marco Baass <picture@marcobaass.com>
2026-07-13 11:41:18 +0200

    fix footer background color and form

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import icon from 'astro-icon';
 
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  site: 'https://www.baass-fotografie.de',
+  trailingSlash: 'always',
+  output: 'static',
   server: {
     host: true
   },
-  output: "static"
 });
